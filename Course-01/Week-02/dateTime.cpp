2a9339ea20fd2591696b56239889364d327ffd95
Varun Kumar <varunjha089@gmail.com>
2021-02-14 05:38:15 +0000

    dateTime*.cpp  file modified and DAYS_IN_WEEK constant literal added in files.

--- Course-01/Week-02/dateTime.cpp
+++ Course-01/Week-02/dateTime.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+const int DAYS_IN_WEEK = 7;
+
 int main(){
     int givenDays;              // total no of days
     int weeks;                  // No of weeks in given days
@@ -11,8 +13,8 @@ int main(){
 
     givenDays = 19;
 
-    weeks = givenDays / 7;      // '/' returns quotient, which will be the no. of weeks.
-    days = givenDays % 7;       // '%' returns remainder, which will be the no. of days. 
+    weeks = givenDays / DAYS_IN_WEEK;      // '/' returns quotient, which will be the no. of weeks.
+    days = givenDays % DAYS_IN_WEEK;       // '%' returns remainder, which will be the no. of days. 
 
     cout << "In " << givenDays << " days, there will be " << weeks << " week and " << days << " days." << endl;
 
--- Course-01/Week-02/dateTimeRepeated.cpp
+++ Course-01/Week-02/dateTimeRepeated.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+const int DAYS_IN_WEEK = 7;
+
 int main(){
     int givenDays;              // total no of days
     int weeks;                  // No of weeks in given days
@@ -16,8 +18,8 @@ int main(){
         cout << "Please enter the no of days." << endl;
         cin >> givenDays;
 
-        weeks = givenDays / 7;      // '/' returns quotient, which will be the no. of weeks.
-        days = givenDays % 7;       // '%' returns remainder, which will be the no. of days. 
+        weeks = givenDays / DAYS_IN_WEEK;      // '/' returns quotient, which will be the no. of weeks.
+        days = givenDays % DAYS_IN_WEEK;       // '%' returns remainder, which will be the no. of days. 
 
         cout << "In " << givenDays << " days, there will be " << weeks << " week and " << days << " days." << endl;    
 
