94747a8b91d76379c22c67a76d856a75a592704b
Varun Kumar <varunjha089@gmail.com>
2021-12-31 06:12:52 +0000

    A simple leap year program is completed.

--- Course-01/Week-05/isleapyearc.cpp
+++ Course-01/Week-05/isleapyearc.cpp
@@ -5,6 +5,25 @@ using namespace std;
 
 int main(){
     int year;
+    cin >> year;
 
-    cout << "";
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0){
+                cout << year << " is a leap year." << endl;
+            }
+            else{
+                cout << year << " is not a leap year." << endl;
+            }
+        }
+        else{
+            cout << year << " is a leap year." << endl;
+        }
+    }
+    else{
+        cout << year << " is not a leap year." << endl;
+    }
+
+
+    return 0;
 }
