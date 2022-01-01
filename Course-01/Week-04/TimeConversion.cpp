93b1002c91b5e8dda13c40e9c7f6a269625f6f61
Varun Kumar <varunjha089@gmail.com>
2022-01-01 05:43:17 +0000

    Time conversion from 24H format to 12H format completed.

---
+++ Course-01/Week-04/TimeConversion.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int hour24, minutes24;
+    int hour12, minutes12;
+    string period;
+    char temp;
+
+    cout << "Please Enter Time in 24-Hour Format." << endl;
+    cin >> hour24 >> temp >> minutes24;
+
+    minutes12 = minutes24;
+    if (hour24 >= 0 && hour24 <= 11){
+        period = "am";
+        if (hour24 == 0){
+            hour12 = 12;
+        }else{
+            hour12 = hour24;
+        }
+    }else{
+        period = "pm";
+        if (hour24 == 12){
+            hour12 = 12;
+        }else{
+            hour12 = hour24 - 12;
+        }
+    }
+
+    cout << "The time is " << hour12 <<":" << minutes12 << " " << period << endl;
+
+    return 0;
+}
