74b255e90db5eb9fe167741a35de55e1b4709149
Varun Kumar <varunjha089@gmail.com>
2022-01-01 04:50:05 +0000

    Character Classification completed.

--- Course-01/Week-04/CharacterClassification.cpp
+++ Course-01/Week-04/CharacterClassification.cpp
@@ -8,5 +8,22 @@ int main(){
     cout << "Please Enter a Character" << endl;
     cin >> user;
 
+    if (user >= 'a' && user <= 'z')
+    {
+        cout << user << " is a small letter." << endl;
+    }
+    else if (user >= 'A' && user <= 'Z')
+    {
+        cout << user << " is a capital letter." << endl;
+    }
+    else if (user >= '0' && user <= '9')
+    {
+        cout << user << " is a digit." << endl;
+    }
+    else
+    {
+        cout << user << " is not alpha-numeric." << endl;
+    }
     
+    return 0;
 }
